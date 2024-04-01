755ac71ce13fb60c796dab371e7a83e765d7f160
parthmanhas <parthmanhas@gmail.com>
2024-04-02 00:06:00 +0530

    fix colors man

--- apps/product-feedback-app/app/components/RoadmapCard.tsx
+++ apps/product-feedback-app/app/components/RoadmapCard.tsx
@@ -2,9 +2,15 @@ import Tag from '@/app/components/Tag';
 import Upvote from '@/app/components/Upvote';
 import CommentsNumber from '@/app/components/CommentsNumber';
 import { Status } from '../interfaces/interface';
+import clsx from 'clsx';
 export default function RoadmapCard({ status, title, subtitle, tags = [], upvotes, commentsCount }: { status: Status, title: string, subtitle: string, tags: string[], upvotes: number, commentsCount: number }) {
     return <div className="mt-8">
-        <div className={`bg-white p-8 rounded-md border-t-8 border-t-${status.color}-500`}>
+        <div className={clsx({
+            "bg-white p-8 rounded-md border-t-8": true,
+            "border-t-yellow-500": status.color === "yellow",
+            "border-t-purple-500": status.color === "purple",
+            "border-t-blue-500": status.color === "blue",
+        })}>
             <div className="flex items-center mb-2">
                 <div className={`w-2 h-2 bg-${status.color}-500 rounded-full mr-3`}></div>
                 <p className="text-waikawa-gray-700">{status.name}</p>
