56809d1a139a9abb893783ec0c670deca1e3f9f6
parthmanhas <parthmanhas@gmail.com>
2024-05-21 20:27:22 +0530

    add empty feedback component

--- apps/product-feedback-app/src/app/page.tsx
+++ apps/product-feedback-app/src/app/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { FeedbackNavigationMenu } from '@/components/navigation-menu';
 import db from "@/lib/db";
 import { FeedbackWithTags } from "@/lib/types";
+import EmptyFeedback from "@/components/empty-feedback";
 
 async function getFeedbacks() {
   const result = await db.feedback.findMany({
@@ -26,25 +27,29 @@ async function getAllTags() {
 
 export default async function Home() {
 
-  const feedbacks = await getFeedbacks();
+  // const feedbacks = await getFeedbacks();
+  const feedbacks = [];
   const allTags = await getAllTags();
 
   return (
-    <main className="flex min-h-screen justify-between max-w-[1280px] mx-auto">
-      <Content className="w-full h-full">
-        <nav className="flex items-center w-full justify-between p-8 bg-black/5 h-[100px]">
-          <h1>Feedback Board</h1>
-          <FeedbackNavigationMenu tags={allTags} />
-          <div>
-            <p className="font-bold text-xl">{feedbacks.length} Suggestions</p>
-          </div>
-          <Button>Add Feedback</Button>
-        </nav>
-        <div className="w-full mt-5 space-y-4 pb-5">
-          {feedbacks?.map((feedback, index) => (
-            <SuggestionCard key={index} feedback={feedback} />
-          ))}
+    <main className="flex flex-col min-h-screen justify-between max-w-[1280px] mx-auto">
+      <nav className="flex items-center w-full justify-between p-8 bg-black/5 h-[100px]">
+        <h1>Feedback Board</h1>
+        <FeedbackNavigationMenu tags={allTags} />
+        <div>
+          <p className="font-bold text-xl">{feedbacks.length} Suggestions</p>
         </div>
+        <Button>Add Feedback</Button>
+      </nav>
+      <Content className="flex-1 flex">
+        {feedbacks.length > 0 &&
+          <div className="w-full mt-5 space-y-4 pb-5">
+            {feedbacks?.map((feedback, index) => (
+              <SuggestionCard key={index} feedback={feedback} />
+            ))}
+          </div>
+        }
+        {feedbacks.length === 0 && <EmptyFeedback />}
       </Content>
     </main>
   );
