e90c9d3ff39bea1cf282c9734ed3691a0f5373e2
parthmanhas <parthmanhas@gmail.com>
2024-06-12 20:33:34 +0530

    fix size of pagination buttons

--- apps/product-feedback-app/src/components/feedback-pagination.tsx
+++ apps/product-feedback-app/src/components/feedback-pagination.tsx
@@ -3,14 +3,13 @@
 import {
     Pagination,
     PaginationContent,
-    PaginationEllipsis,
     PaginationItem,
     PaginationLink,
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function FeedbackPagination({ feedbacksCount }: { feedbacksCount: number }) {
     const MAX = Math.ceil(feedbacksCount / 10);
@@ -26,16 +25,21 @@ export function FeedbackPagination({ feedbacksCount }: { feedbacksCount: number 
         replace(`${pathname}?${params.toString()}`);
     }
 
-    const [currentPage, setCurrentPage] = useState(+(searchParams?.get('page') || 1));
+    const currentPageFromQueryParams = searchParams.has('page') ? Number(searchParams.get('page')) : 1;
+    const [currentPage, setCurrentPage] = useState(currentPageFromQueryParams);
 
     const handlePreviousClick = () => {
-        setCurrentPage(currentPage - 1 < 1 ? 1 : currentPage - 1);
-        handlePaginationClick(currentPage - 1 < 1 ? 1 : currentPage - 1);
+        if (currentPage === 1) return;
+        const newPage = currentPage - 1 < 1 ? 1 : currentPage - 1
+        setCurrentPage(newPage);
+        handlePaginationClick(newPage);
     }
 
     const handleNextClick = () => {
-        setCurrentPage(currentPage + 1 > MAX ? MAX : currentPage + 1);
-        handlePaginationClick(currentPage + 1 > MAX ? MAX : currentPage + 1);
+        if (currentPage === MAX) return;
+        const newPage = currentPage + 1 > MAX ? MAX : currentPage + 1;
+        setCurrentPage(newPage);
+        handlePaginationClick(newPage);
     }
 
     return (
@@ -44,11 +48,13 @@ export function FeedbackPagination({ feedbacksCount }: { feedbacksCount: number 
                 <PaginationItem>
                     <PaginationPrevious onClick={handlePreviousClick} />
                 </PaginationItem>
-                {Array.from({ length: MAX }, (_, i) => i + 1).map((item, index) => (
-                    <PaginationItem key={index}>
-                        <PaginationLink isActive={searchParams?.get('page') == `${item}`} onClick={() => handlePaginationClick(item)}>{item}</PaginationLink>
-                    </PaginationItem>
-                ))}
+                <div className="max-w-[200px] overflow-x-auto flex">
+                    {Array.from({ length: MAX }, (_, i) => i + 1).map((item, index) => (
+                        <PaginationItem key={index}>
+                            <PaginationLink isActive={searchParams?.get('page') == `${item}`} onClick={() => handlePaginationClick(item)}>{item}</PaginationLink>
+                        </PaginationItem>
+                    ))}
+                </div>
                 <PaginationItem>
                     <PaginationNext onClick={handleNextClick} />
                 </PaginationItem>
